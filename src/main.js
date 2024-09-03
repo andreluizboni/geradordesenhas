207c0cc9a83f0e9be95c2678ccf68dfa85793c57
André Luiz Moreira Boni <andreluiz89@gmail.com>
2024-09-03 17:00:03 -0300

    base pronta

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+
+const passwordElement = document.querySelector('button');
+
+const displayPassword = document.querySelector('h2');
+
+passwordElement.addEventListener('click', () => {
+  const password = nanoid();
+  displayPassword.innerHTML = password;
+});
+
+displayPassword.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
+});
